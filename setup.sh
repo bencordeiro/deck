2c2d5bfa8a50d188926c254e181827c7a43cdfc6
bencordeiro <94664097+bencordeiro@users.noreply.github.com>
2022-12-27 18:18:37 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#script made by Ben Cordeiro
+
+sudo steamos-readonly disable
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+
+#yes | sudo pacman -S refind
+#sudo refind-install
