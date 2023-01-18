992cca35a515bd0cc81fb54987af7c9e0119de6e
bencordeiro <94664097+bencordeiro@users.noreply.github.com>
2023-01-18 16:33:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@
 sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-pacman -Syu
 
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+cat /etc/pacman.conf | grep sublime || echo sublime not found in /etc/pacman.conf
+sudo pacman -Syu sublime-text
 sudo pacman -S hashcat
+pacman -Syu
 #sudo pacman -S (app-here)
 #yes | sudo pacman -S refind
 #sudo refind-install
