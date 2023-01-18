bc918ed2ef6767c74d0ec0adfa33da1325a141ee
bencordeiro <94664097+bencordeiro@users.noreply.github.com>
2023-01-18 16:22:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 sudo steamos-readonly disable
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
+pacman -Syu
 
+sudo pacman -S hashcat
+#sudo pacman -S (app-here)
 #yes | sudo pacman -S refind
 #sudo refind-install
